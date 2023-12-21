df15f242fe748f60a798b707d4d18a4a67f34f18
Abd-Elrahman Qasem <133685718+Abdulrahman-sherif@users.noreply.github.com>
2023-12-21 04:34:31 +0200

    Add files via upload

---
+++ Task-3/part - 2/التكليف 01/main.js
@@ -0,0 +1,9 @@
+let Number_One = 10,
+  Number_Two = 20;
+console.log(1020);
+console.log("10" + "20");
+console.log(`${Number_One}${Number_Two}`);
+console.log(`1020`)
+console.log(Number_Two + "\n" + Number_One)
+console.log(`${Number_Two}
+${Number_One}`)
